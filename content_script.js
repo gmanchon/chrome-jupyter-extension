377e3097f613d3848ad4186ed9fd1065e568744a
Gaetan Manchon <gmanchon@gmail.com>
2021-06-15 14:57:38 +0200

    correct margin when dev tool is opened

--- content_script.js
+++ content_script.js
@@ -2,6 +2,6 @@
 window.onload = function() {
 
   // distraction free mode
-  document.getElementById("notebook-container").style.margin = 0
+  document.getElementById("notebook-container").style.margin = "auto"
   document.getElementById("notebook-container").style.width = "100%"
 }
